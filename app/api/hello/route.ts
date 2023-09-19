c3796559aa2655efa7a4c79f8a17e62cc82175fa
astertechs-dev <astertechs.dev@gmail.com>
2023-09-19 11:16:37 +0900

    chore: [main] test app router api.

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+// Next.js 13 より App Router を使用したルーティングに変更したため、APIを試験的に作成
+// 確認時は、エンドポイントを "/api/hello" としてAPIをコールする。 
 export async function GET() {
   return NextResponse.json({ data:"Hello Next.js 13 !!!" });
 }
