53f818a1839600bca470dfe1816d853281c1890f
astertechs-dev <astertechs.dev@gmail.com>
2023-09-19 11:31:43 +0900

    feat: [main] add search area in navbar.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Container from "../Container"
 import Logo from "./Logo"
+import Search from "./Search"
 
 const Navbar = () => {
   return (
@@ -19,6 +20,7 @@ const Navbar = () => {
             '
           >
             <Logo />
+            <Search />
           </div>
         </Container>
       </div>
